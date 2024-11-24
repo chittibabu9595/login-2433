f20aee86a6e0f11528580270c60678f0f1ac8020
chittibabu9595 <chittibabu1836@gmail.com>
2024-11-24 13:22:58 +0000

    Create script2 web app deployment

--- script2.sh
+++ script2.sh
@@ -16,8 +16,11 @@ echo "Remove Sample Pages"
 sudo rm -rf /var/www/html
 sleep 5
 
+echo "Create a new document"
+sudo mkdir -p /var/www/html
+
 echo "Clone Login App"
-sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
+sudo git clone https://github.com/chittibabu9595/login-2433.git /var/www/html
 sleep 5
 
 echo "Browse Login App with Server Public IP"
