a4d91384a61a4c664d0ce6ed249faca00e792d12
Chittibabu9595 <chittibabu1836@gmail.com>
2025-04-28 08:23:30 +0530

    terraform1

--- proj_modules/login_app.tf
+++ proj_modules/login_app.tf
@@ -1,5 +1,5 @@
 module "login_vpc" {
-  source             = "./modules/vpc"
+  source             = "./vpc"
   vpc_cidr           = "10.0.0.0/16"
   public_subnet_cidr = "10.0.1.0/24"
   availability_zone  = "us-west-2a"
@@ -7,7 +7,7 @@ module "login_vpc" {
 }
 
 module "login_ec2" {
-  source                 = "./modules/ec2"
+  source                 = "./ec2"
   ami                    = "ami-03f8acd418785369b"
   instance_type          = "t2.micro"
   subnet_id              = module.login_vpc.public_subnet_id
